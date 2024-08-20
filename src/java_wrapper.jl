2f441cb7a6e7e0a0e61079ac9df07c691393e6a1
Azzaare <jf@baffier.fr>
2024-08-20 01:25:16 +0000

    Update JavaCall to 0.8. Doesn't fix the test errors

--- src/java_wrapper.jl
+++ src/java_wrapper.jl
@@ -6,29 +6,23 @@
 # These correspond to Java types (which are directly returned by the functions
 # in this binder). In particular, the inheritance defined in Java is not brought
 # back to Julia.
-const Store = JavaObject{Symbol("org.jacop.core.Store")}
+const Store = @jimport org.jacop.core.Store
 
-const BooleanVar = JavaObject{Symbol("org.jacop.core.BooleanVar")}
-const IntVar = JavaObject{Symbol("org.jacop.core.IntVar")}
-const SetVar = JavaObject{Symbol("org.jacop.set.core.SetVar")}
-const FloatVar = JavaObject{Symbol("org.jacop.floats.core.FloatVar")}
-const CircuitVar = JavaObject{Symbol("org.jacop.constraints.CircuitVar")}
+const BooleanVar = @jimport org.jacop.core.BooleanVar
+const IntVar = @jimport org.jacop.core.IntVar
+const SetVar = @jimport org.jacop.set.core.SetVar
+const FloatVar = @jimport org.jacop.floats.core.FloatVar
+const CircuitVar = @jimport org.jacop.constraints.CircuitVar
 
-const Constraint = JavaObject{Symbol("org.jacop.constraints.Constraint")}
-const LinearInt = JavaObject{Symbol("org.jacop.constraints.LinearInt")}
-const Alldifferent = JavaObject{Symbol("org.jacop.constraints.Alldifferent")}
-const In = JavaObject{Symbol("org.jacop.constraints.In")}
-const XeqC = JavaObject{Symbol("org.jacop.constraints.XeqC")}
+const Constraint = @jimport org.jacop.constraints.Constraint
+const LinearInt = @jimport org.jacop.constraints.LinearInt
+const Alldifferent = @jimport org.jacop.constraints.Alldifferent
+const In = @jimport org.jacop.constraints.In
+const XeqC = @jimport org.jacop.constraints.XeqC
 
 # Unions of types to model Java type hierarchy.
 
-const Variable = Union{
-    BooleanVar,
-    IntVar,
-    SetVar,
-    FloatVar,
-    CircuitVar,
-}
+const Variable = Union{BooleanVar, IntVar, SetVar, FloatVar, CircuitVar}
 
 # Add a constraint to a store.
 
